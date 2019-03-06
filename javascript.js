d7f8acd1572cf71a62ab6bce66aa409b892a1bc6
Ellebouncer <eliassept6@program.st>
2019-03-06 01:33:18 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    //the constant temprature in kelvin
+    let kelvin = prompt("Degrees kelvin:");
+            //degrees celsius equals degrees kelvin - 273
+            let celsius = kelvin - 273;
+            //equation for celsius to fahrenheit
+            let fahrenheit = celsius * (9 / 5) + 32;
+            //make fahrenheit into an integer
+            fahrenheit = Math.floor(fahrenheit);
+            document.write(`The temperature is ${fahrenheit} degrees Fahrenheit. `);
+            newton = celsius * (33 / 100);
+            newton = Math.floor(newton)
+            document.write(`It's ${newton} degrees newton. `)
+            document.write(`It's ${celsius} degrees celsius.`)
+}
+function myFunction2(){
+    let myAge = prompt("Your age(in human years):");
+    let earlyYears = 2;
+    earlyYears *= 10.5;
+    let laterYears = myAge - 2;
+    laterYears *= 4;
+    let myDogAge = earlyYears + laterYears;
+    document.write(myDogAge)
+}
